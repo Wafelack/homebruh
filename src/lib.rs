f9ab421ccb1ddbd09c6032fac2c98d57d09f77a5
Wafelack <wafelack@protonmail.com>
2020-12-12 20:43:41 +0100

    feat: complete rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,7 @@
-pub mod maps;
-pub mod entities;
-use maps::map::*;
-use entities::entity::*;
-
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    fn emptymap() {
-        let map = GameMap::new(50,8,'#');
-        map.create();
-    }
-    #[test]
-    fn resizing() {
-        let mut map = GameMap::new(50,8,'#');
-        map.create();
-        map.setsize(40, 8);
-        map.create();
-    }
-    #[test]
-    fn changedelimiter() {
-        let mut map = GameMap::new(50,8,'#');
-        map.create();
-        map.setdelimiter('*');
-        map.create();
-    }
-    #[test]
-    fn entities() {
-        let mut map = GameMap::new(50, 8, '#');
-        let mut entities: Vec<Entity> = vec![Entity::new(10, 3, false, '8', false),Entity::new(15, 3, false, '$', false)];
-        map.drawentities(&entities);
-        entities[0].move_to(10, 5);
-        map.drawentities(&entities);
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
     }
 }
