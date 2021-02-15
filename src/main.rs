99e4c6a9af4c4fde38287a49faa070536412acf8
Wafelack <wafelack@protonmail.com>
2021-02-15 22:21:46 +0100

    :sparkles: Building build command.

--- src/main.rs
+++ src/main.rs
@@ -1,111 +1,85 @@
-mod init;
-mod utils;
-mod packages;
+use packager::{builder::build, unbuilder::unbuild};
 
-use clap::{Arg,App, SubCommand};
-use packages::{install, search, purge,upgrade};
+mod packager;
 
-fn main() -> anyhow::Result<()> {
-    let matches = App::new("werb")
-                    .version(env!("CARGO_PKG_VERSION"))
-                    .author(env!("CARGO_PKG_AUTHORS"))
-                    .about("Yet another rusty package manager")
-                    .subcommand(SubCommand::with_name("search")
-                            .arg(Arg::with_name("package")
-                                    .required(true)
-                                    .takes_value(true)
-                                    .index(1))
-                    )
-        .subcommand(SubCommand::with_name("purge")
-            .arg(Arg::with_name("package")
-                .index(1)
-                .required(true)
-                .takes_value(true)))
-                    .subcommand(SubCommand::with_name("install")
-                        .arg(Arg::with_name("package")
-                            .index(1)
-                            .required(true)
-                            .takes_value(true)
-                        )
-                        .arg(Arg::with_name("force")
-                            .short("f")
-                            .long("force")
-                            .takes_value(false)))
+fn main() -> Result<()> {
+    let args = std::env::args().skip(1).collect::<Vec<String>>();
 
-        .subcommand(SubCommand::with_name("upgrade"))
-                    .get_matches();
-    init::init()?;
-
-    if let Some(matches) = matches.subcommand_matches("search") {
-        println!("{}", search(matches.value_of("package").unwrap())?);
-    } else if let Some(matches) = matches.subcommand_matches("install") {
-        install(matches.value_of("package").unwrap(), !matches.is_present("force"), true)?;
-    } else if let Some(matches) = matches.subcommand_matches("purge") {
-        purge(&matches.value_of("package").unwrap().to_owned(), true)?;
-    } else if let Some(_) = matches.subcommand_matches("upgrade") {
-        upgrade()?;
+    if args.len() > 0 {
+        if args.contains(&"--help".to_owned()) {
+            help();
+        } else if args.contains(&"--version".to_owned()) {
+            println!("{} {}",env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+        } else {
+            match args[0].as_str() {
+                "build" => if args.len() == 2 {
+                    build(&args[1])?;
+                } else {
+                    println!("Usage: {} build <manifest>.", env!("CARGO_PKG_NAME"));
+                },
+                "unpack" => if args.len() == 2 {
+                    unbuild(&args[1])?;
+                } else {
+                    println!("Usage: {} unpack <package>.", env!("CARGO_PKG_NAME"));
+                }
+                _ => {}
+            }
+        }
     }
 
     Ok(())
 }
 
-#[cfg(test)]
-mod test {
-    use crate::utils::*;
-    use super::*;
+fn help() {
+    println!("{} {}", env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"));
+    println!("{}", env!("CARGO_PKG_AUTHORS"));
+    println!("{}", env!("CARGO_PKG_DESCRIPTION"));
+
+    // Usage
+    println!("\nUSAGE:");
+    println!("\n{} <COMMAND> [OPTIONS]", env!("CARGO_PKG_NAME"));
+
+    // Flags
+    println!("\nFLAGS:");
+    println!("\t--help   \tDisplays this message.");
+    println!("\t--version\tDisplays version information.");
+
+    // Commands
+    println!("\nCOMMANDS:");
+    println!("\tbuild $manifest\tBuilds the specified package manifest.");
+
+    println!();
+}
+
+#[derive(Debug)]
+pub enum Error {
+    IoError(std::io::Error),
+    TomlError(TomlError),
+    OtherError(String),
+}
+#[derive(Debug)]
+pub enum TomlError {
+    DeError(toml::de::Error),
+    SerError(toml::ser::Error)
+}
+
+pub type Result<T> = std::result::Result<T, Error>;
 
 
-    #[test]
-    fn installation() ->  anyhow::Result<()> {
-        init::init()?;
-        packages::install("nixt", false,true)?;
-        Ok(())
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Self::IoError(e)
     }
-    #[test]
-    fn search_package() -> anyhow::Result<()> {
-        let packages = get_packages("packages.json")?;
+}
 
-        let mut has_to_be_here: Option<Package> = None;
-
-        for package in packages {
-            if package.name == "nixt" {
-                has_to_be_here = Some(package);
-            }
-        }
-
-        assert!(has_to_be_here.is_some());
-        Ok(())
+impl From<toml::ser::Error> for Error {
+    fn from(e: toml::ser::Error) -> Self {
+        Self::TomlError(TomlError::SerError(e))
     }
+}
 
-    #[test]
-    fn package_serialization() {
-        let to_ser = Package::new(
-            "wng",
-            vec!["Wafelack <wafelack@protonmail.com>"],
-            "3.5.0",
-            "N/A",
-            "Wanager is a package and projects manager for C and C++"
-        );
-        let serialized = serde_json::to_string(&to_ser).unwrap();
-        assert_eq!(
-            serialized,
-            r#"{"name":"wng","description":"Wanager is a package and projects manager for C and C++","authors":["Wafelack <wafelack@protonmail.com>"],"version":"3.5.0","source":"N/A"}"#
-        );
-    }
-
-    #[test]
-    fn package_deserialization() {
-        let to_deser = r#"{"name":"wng", "description":"Wanager is a package and projects manager for C and C++", "authors":["Wafelack <wafelack@protonmail.com>"],"version":"3.5.0","source":"N/A"}"#;
-        let deserialized: Package = serde_json::from_str(&to_deser).unwrap();
-        assert_eq!(
-            deserialized,
-            Package::new(
-                "wng",
-                vec!["Wafelack <wafelack@protonmail.com>"],
-                "3.5.0",
-                "N/A",
-                "Wanager is a package and projects manager for C and C++"
-            )
-        );
+impl From<toml::de::Error> for Error {
+    fn from(e: toml::de::Error) -> Self {
+        Self::TomlError(TomlError::DeError(e))
     }
 }
